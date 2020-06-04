0fcd3c01abc55af79bc6b66531f68ceb100473e0
Godwin Chiezugo <gchiezugo47@gmail.com>
2020-06-04 20:01:44 +0100

    just in

--- scripts/HNG-01889.js
+++ scripts/HNG-01889.js
@@ -2,9 +2,10 @@ let intern = {
     "greeting": "Hello World, ", 
     "name": "Godwin Chiezugo ",
     "ID": "HNG-01889 ",
+    "email": "gchiezugo47@gmail.com ",
     "language": "JavaScript ",
     internInfo: function (){
-        return this.greeting + 'this is ' + this.name + 'with HNGi7 ID ' + this.ID + 'using ' + this.language + 'for stage 2 task';
+        return this.greeting + 'this is ' + this.name + 'with HNGi7 ID ' + this.ID + 'and email ' + this.email +'using ' + this.language + 'for stage 2 task';
     }
 };
 console.log(intern.internInfo());
